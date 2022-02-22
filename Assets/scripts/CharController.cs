f922daa0d67220289295e277ffec407dd803ce7a
Roman <barbarista1862@gmail.com>
2022-02-22 12:36:01 +0300

    Initial commit

---
+++ Assets/scripts/CharController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    [SerializeField] private GameObject _camera;
+    [SerializeField] private float _speed = 6;
+    [SerializeField] private float _sideSpeed = 2;
+    [SerializeField] private float _deadZoneRotation = 10;
+
+    private Rigidbody _player;
+
+    void Start()
+    {
+        _player = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 dir = _player.velocity;
+        if(_camera.transform.rotation.eulerAngles.z > _deadZoneRotation
+            && _camera.transform.rotation.eulerAngles.z <= 180)
+        {
+            dir.x = _camera.transform.rotation.eulerAngles.z * -1 * Time.deltaTime * _sideSpeed;
+        }
+
+        if (_camera.transform.rotation.eulerAngles.z > 180
+            && _camera.transform.rotation.eulerAngles.z <= 1360 - _deadZoneRotation)
+        {
+            dir.x = (360 - _camera.transform.rotation.eulerAngles.z) * -1 * Time.deltaTime * _sideSpeed;
+        }
+
+        dir.x = Input.GetAxis("Horizontal") * _sideSpeed;
+        dir.z = _speed;
+        _player.velocity = dir;
+
+    }
+}
