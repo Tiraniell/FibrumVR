e38b5174b331e86d02eebcb1a12eb7f76b3c205f
Roma Ag <93056140+Tiraniell@users.noreply.github.com>
2022-02-28 02:33:57 +0300

    new commit
    next commit

---
+++ Assets/scripts/ObstacleKiller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleKiller : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    [SerializeField] private ObstacleSpawner _spawner;
+    [SerializeField] private float _killDistanzeZ;
+
+
+    void Update()
+    {
+        List<Transform> obstacles = _spawner.spawnedObstacles;
+
+        for( int i = 0; i < obstacles.Count; i++)
+        {
+            if (_player.position.z > obstacles[i].position.z + _killDistanzeZ)
+            {
+                Destroy(obstacles[i].gameObject);
+            }
+        }
+    }
+}
