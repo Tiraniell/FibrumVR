559f30b2d08b8a658748e87cdd7f11ea5364147a
Roma Ag <93056140+Tiraniell@users.noreply.github.com>
2022-02-27 15:09:03 +0300

    new commit
    new script

---
+++ Assets/scripts/ObstacleSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] _obstacle;
+    [SerializeField] private float _spawnStep;
+    [SerializeField] private float _spawnDictance;
+
+    [SerializeField] private Vector2 _sergmentWidth;
+
+    private Transform _myTrans;
+    private Vector3 _lastPos;
+
+    private List<Transform> _spawnedObstacles = new List<Transform>();
+    public List<Transform> spawnedObstacles { get { _spawnedObstacles.RemoveAll(TransformIsNull); return _spawnedObstacles;  } }
+
+    bool TransformIsNull (Transform a)
+    {
+        return a == null;
+    }
+
+    void Start()
+    {
+        _myTrans = transform;
+        _lastPos = _myTrans.position;
+        
+    }
+
+    void Update()
+    {
+        if(_myTrans.position.z > _lastPos.z + _spawnStep)
+        {
+            _lastPos.z += _spawnStep;
+
+            Transform newObstacle = _obstacle[Random.Range(0, _obstacle.Length)];
+            _spawnedObstacles.Add(Instantiate(newObstacle, new Vector3(
+                Random.Range(_sergmentWidth.x, _sergmentWidth.y), 0,
+                _lastPos.z + _spawnDictance), Quaternion.identity));
+             
+        }
+        
+    }
+}
